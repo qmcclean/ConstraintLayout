f4e383a061bb9b3227357e80a66cbe62d021d558
Quinnion McClean <que@Quinnions-MacBook-Pro.local>
2020-02-16 21:44:48 -0500

    working with constraint layout in the editor. Powerful stuff using biases, chaining and baselines

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setClickListener()
+    }
+
+    fun setClickListener() {
+        val clickableViews: List<View> = listOf(
+            box_one_text,
+            box_two_text,
+            box_three_text,
+            box_four_text,
+            box_five_text,
+            red_button,
+            yellow_button,
+            green_button
+        )
+
+        for (view in clickableViews) {
+            view.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.colorRed)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.colorYellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.colorGreen)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
